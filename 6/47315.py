46c990deb29a479e64a0bedae3ef5e2333ca9113
link-pc <sm171105@bk.ru>
2023-06-09 13:16:04 +0300

    add new work

--- 6/47315.py
+++ 6/47315.py
@@ -1,11 +1,11 @@
 from turtle import *
 
-color('black', 'green')
+color('green', 'green')
 left(90)
 k = 30
 
 begin_fill()
-for i in range(2):
+for i in range(6):
     forward(7*k)
     right(90)
     forward(7*k)
@@ -20,7 +20,7 @@ for x in range(-100*k, 100*k, k):
         if s:
             ind = s[0]
             color = canv.itemcget(ind, 'fill')
-            if color == 'green' and len(s) == 1:
+            if color == 'green' and len(s) >= 1:
                 c += 1
 print(c)
 done()
